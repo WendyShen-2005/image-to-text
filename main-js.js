5786f1ed46bae6c7376c24c902907de44a6402ca
Wendy <wendyshen2005@gmail.com>
2024-07-02 01:56:13 -0400

    finished

--- main-js.js
+++ main-js.js
@@ -1,12 +1,22 @@
-import { response } from "express";
+var textWidth = 50;
+var copyText = "";
+
+handleWidthChange = () => {
+    textWidth = document.getElementById("width-change").value
+    document.getElementById("generate-button").innerHTML = "Generate Text (current text width: " + textWidth + ")"
+}
 
 const handleImgChange = (url) => {
+    document.getElementById("img-title").innerHTML = "Image to be processed:"
+    document.getElementById("generate-button").disabled = false
+
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     const img = new Image();
     // img.crossOrigin = 'Anonymous'; // Set the crossOrigin attribute
     img.src = url;
     img.addEventListener('load', function() {
+        canvas.style.height = img.height / img.width + "%"
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
@@ -14,35 +24,6 @@ const handleImgChange = (url) => {
 
     document.getElementsByTagName("html")[0].style.height = 'fit-content';
 }
-document.addEventListener("DOMContentLoaded", function () {
-    // var corsAttr = new EnableCorsAttribute("*", "*", "*");
-    // config.EnableCors(corsAttr);
-
-
-    fetch('init.png')
-        .then(response => response.blob())
-        .then(blob => {
-            const reader = new FileReader();
-
-            reader.addEventListener('load', function() {
-                localStorage.setItem('img', reader.result);
-                handleImgChange(localStorage.getItem('img'));
-            }, false);
-
-            reader.readAsDataURL(blob);
-        })
-        .catch(e => console.error('error fetching image', error));
-
-    // const reader = new FileReader();
-
-    // reader.addEventListener('load', function() {
-    //     localStorage.setItem("img", reader.result);
-    //     handleImgChange(localStorage.getItem("img"))
-    // }, false)
-
-    // if ('init.png') 
-    //     reader.readAsDataURL('init.png')
-})
 
 
 handleInputChange = () => {
@@ -50,55 +31,76 @@ handleInputChange = () => {
     const reader = new FileReader();
 
     reader.addEventListener('load', function() {
-        localStorage.setItem("img", reader.result);
-        handleImgChange(localStorage.getItem("img"))
+        localStorage.setItem("imgURL", reader.result);
+        handleImgChange(localStorage.getItem("imgURL"))
     }, false)
 
     if (file) 
         reader.readAsDataURL(file)
 }
 
-handleCopyClick = () => {
-    var copyText = document.getElementById("generated-text").innerHTML;
+handleCopyClick = () => {   
+    document.getElementById("copied").style.display = "inline"; 
     navigator.clipboard.writeText(copyText)
-    alert("Copied!")
 }
 
 rgbToGrayScale = (r, g, b) => {
-    return r * 0.299 + g * 0.587 + b * 0.114
+    return Math.floor(r * 0.299 + g * 0.587 + b * 0.114)
+}
+
+convertNumberToBrightness = (num) => {
+        if(num >= 225) return "☐"
+        if(num >= 200) return "◰"
+        if(num >= 175) return "◫"
+        if(num >= 150) return "▥"
+        if(num >= 125) return "◨"
+        if(num >= 100) return "◩"
+        if(num >= 75) return "▣"
+        if(num >= 50) return "▦"
+        if(num >= 25) return "▩"
+        else return "■."
 }
 
 handleGenerateTextClick = () => {
+    document.getElementById("copied").style.display = "none"; 
+    document.getElementById("copy-button").disabled = false
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    const allData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const allData = ctx.getImageData(0, 0, canvas.width, canvas.height).data
 
-    const text = "";
+    var htmlText = "";
+    copyText = ""
 
-    const textWidth = 100;
-
-    if(canvas.width <= textWidth) {
+    if(canvas.width <= textWidth) {  // inputted width is greater than pixel width, will default to pixel width
+        console.log("1")
         for (i = 0; i < canvas.height; i++) {
             for (j = 0; j < canvas.width; j++) {
                 index = (i * canvas.width + j) * 4
-                text += rgbToGrayScale(allData[index], allData[index + 1], allData[index + 2])
+                symbol = convertNumberToBrightness(
+                    rgbToGrayScale(allData[index], allData[index + 1], allData[index + 2]))
+                htmlText += symbol
+                copyText += symbol
             }
-            text += "\n"
+            htmlText += "<br>"
+            copyText += "\n"
         }
     } else {
-        ySkip = Math.floor(canvas.height / textWidth)
-        xSkip = Math.floor(canvas.width / textWidth)
+        skip = canvas.width / textWidth;
 
-        for (i = 0; i < ySkip; i++) {
-            for (j = 0; j < xSkip; j++) {
-                index = ((i * ySkip) * canvas.width + (j * xSkip)) * 4
-                text += rgbToGrayScale(allData[index], allData[index + 1], allData[index + 2])
+        for (i = 0; i < canvas.height; i += skip) {
+            console.log(Math.floor(i )* 4)
+            for (j = 0; j < canvas.width; j += skip) {
+                index = (Math.floor(i) * canvas.width + Math.floor(j)) * 4
+                value = rgbToGrayScale(allData[index], allData[index + 1], allData[index + 2]);
+                symbol = convertNumberToBrightness(value);
+                htmlText += symbol
+                copyText += symbol
             }
-            text += "\n"
+            htmlText += "<br>"
+            copyText += "\n"
         }
     }
 
-
-    console.log(text)
+    document.getElementById("generated-text").innerHTML = htmlText
 }
